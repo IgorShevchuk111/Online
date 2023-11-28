8cd09e33611b59a25890843735469f3a426efc45
Igor Shevchuk <igorshevchuk111@gmail.com>
2023-11-28 17:30:20 +0000

    wip

--- src/components/HeaderComponents/MenuComponents/Menu.js
+++ src/components/HeaderComponents/MenuComponents/Menu.js
@@ -1,36 +1,34 @@
-import React, { useState } from 'react';
-import './Menu.css'
-import {menu} from '../../../header-data/menuData.js'
-
+import React, { useState } from "react";
+import "./Menu.css";
+import { menu } from "../../../header-data/menuData.js";
 
 function Menu() {
-    const [menuVisible, setMenuVisible] = useState(false);
-    const toggleMenu = () => {
+  const [menuVisible, setMenuVisible] = useState(false);
+  const toggleMenu = () => {
     setMenuVisible(!menuVisible);
-    const mainContainer = document.querySelector('.main');
+    const mainContainer = document.querySelector(".main");
     if (mainContainer) {
-      mainContainer.classList.toggle('menu-open', !menuVisible);
+      mainContainer.classList.toggle("menu-open", !menuVisible);
     }
   };
-    
+
   return (
-    <div className='menu-container body-2'>
-      <div className=" material-icons" onClick={toggleMenu}>{menuVisible ? 'close' : 'menu'}</div>
-      {menuVisible && (<div className='menu-items'>{menu.map((item, index) => (
-        <div key={index}>
-          {item.name}
+    <div className="menu-container body-2">
+      <div className=" material-icons" onClick={toggleMenu}>
+        {menuVisible ? "close" : "menu"}
+      </div>
+      {menuVisible && (
+        <div className="menu-items">
+          {menu.map((item, index) => (
+            <div key={index}>{item.name}</div>
+          ))}
         </div>
-      ))}</div>
-      
       )}
       {menu.map((item, index) => (
-        <div key={index}>
-          {item.name}
-        </div>
+        <div key={index}>{item.name}</div>
       ))}
-    </div> 
-    
-  )
+    </div>
+  );
 }
 
 export default Menu;
