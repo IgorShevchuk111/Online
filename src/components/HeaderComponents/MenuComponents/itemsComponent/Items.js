70429145f286f22126f18d89e493cba65deda383
Igor Shevchuk <igorshevchuk111@gmail.com>
2023-12-06 16:50:44 +0000

    Navigation. to  hide content based on the current URL, useLocation hook, Error pag

--- src/components/HeaderComponents/MenuComponents/itemsComponent/Items.js
+++ src/components/HeaderComponents/MenuComponents/itemsComponent/Items.js
@@ -1,6 +1,7 @@
 import React, { useState, useContext } from "react";
 import "./Items.css";
 import { AppContext } from "../../../../App";
+import { Link } from "react-router-dom";
 
 function Items(props) {
   const toggleShowItem = useContext(AppContext).toggleShowItem;
@@ -42,12 +43,14 @@ function Items(props) {
               </div>
               {selectedBrand === brand.brand && (
                 <div className="phones-brands-model">
-                  <div className="see-all-items">
-                    <small>See all</small>
-                    <small className="material-symbols-outlined">
-                      chevron_right
-                    </small>
-                  </div>
+                  <Link to="/allApplePhones">
+                    <div className="see-all-items">
+                      <small>See all</small>
+                      <small className="material-symbols-outlined">
+                        chevron_right
+                      </small>
+                    </div>
+                  </Link>
                   {brand.model.map((model, modelIndex) => (
                     <div onClick={showModel} key={modelIndex}>
                       {model}
--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,20 @@
 import React from "react";
+import ErrorPage from "./components/ErrorPage";
 
 const LazyApp = React.lazy(() => import("./App"));
+const LazyAllPhones = React.lazy(() =>
+  import(
+    "./components/HeaderComponents/MenuComponents/itemsComponent/phones/AllApplePhones"
+  )
+);
 
-const routes = [{ path: "/", element: <LazyApp /> }];
+const routes = [
+  {
+    path: "/",
+    element: <LazyApp />,
+    children: [{ path: "allApplePhones", element: <LazyAllPhones /> }],
+  },
+  { path: "*", element: <ErrorPage /> },
+];
 
 export default routes;
