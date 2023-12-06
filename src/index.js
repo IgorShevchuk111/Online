b7fcf3248577f964c6992983ec1577ad63768d9a
Igor Shevchuk <igorshevchuk111@gmail.com>
2023-12-06 09:49:20 +0000

    refactoring router, created routes.js file

--- src/index.js
+++ src/index.js
@@ -1,7 +1,21 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import App from "./App";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import routes from "./routes";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<App />);
+root.render(
+  <Router>
+    <Routes>
+      {routes.map(({ path, element, children }, index) => (
+        <Route key={index} path={path} element={element}>
+          {children &&
+            children.map(({ path, element }, index) => (
+              <Route key={index} path={path} element={element} />
+            ))}
+        </Route>
+      ))}
+    </Routes>
+  </Router>
+);
