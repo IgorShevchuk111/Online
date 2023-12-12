614cb389ded130f627b9dc8d9587fb5594b2024a
Igor Shevchuk <igorshevchuk111@gmail.com>
2023-12-12 22:04:28 +0000

    refactoring

--- src/components/HeaderComponents/Logo/Logo.js
+++ src/components/HeaderComponents/Logo/Logo.js
@@ -2,10 +2,13 @@ import React from "react";
 import "./Logo.css";
 import { Link } from "react-router-dom";
 
-function Logo() {
+function Logo({ updateSelectedMenuItem }) {
+  const handleClick = () => {
+    updateSelectedMenuItem(false);
+  };
   return (
     <div>
-      <Link className="logo" to="/">
+      <Link className="logo" to="/" onClick={handleClick}>
         <h4 className="logo">My Shop</h4>
       </Link>
     </div>
