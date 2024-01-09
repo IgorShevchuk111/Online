f0a1ebfa95b06ca227ed3ab982eb44db0eb15eb7
Igor Shevchuk <igorshevchuk111@gmail.com>
2024-01-09 13:50:54 +0000

    changed logo

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,12 +1,12 @@
-import React from 'react'
-import './Footer.css'
+import React from "react";
+import "./Footer.css";
 
 function Footer() {
   return (
     <div className="footer-container body-2">
-        &copy; {new Date().getFullYear()} My Shop
+      &copy; {new Date().getFullYear()} Online
     </div>
-  )
+  );
 }
 
-export default Footer
+export default Footer;
